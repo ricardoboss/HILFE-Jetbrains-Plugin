2794bfd08bd239a8cd3ca63309df9441d557eedf
Ricardo Boss <contact@ricardoboss.de>
2023-07-26 01:49:48 +0200

    Add custom server definition and work on dynamic server path

---
+++ src/main/kotlin/de/ricardoboss/plugins/hilfe/HilfeServerDefinition.kt
@@ -0,0 +1,42 @@
+﻿package de.ricardoboss.plugins.hilfe
+
+import com.intellij.ide.plugins.PluginManager
+import com.intellij.openapi.application.PathManager
+import com.intellij.openapi.extensions.PluginId
+import org.wso2.lsp4intellij.client.connection.ProcessStreamConnectionProvider
+import org.wso2.lsp4intellij.client.connection.StreamConnectionProvider
+import org.wso2.lsp4intellij.client.languageserver.serverdefinition.LanguageServerDefinition
+import java.nio.file.Paths
+import java.util.*
+import kotlin.io.path.pathString
+
+class HilfeServerDefinition : LanguageServerDefinition() {
+    init {
+        ext = "hil"
+        languageIds = Collections.emptyMap()
+    }
+
+    override fun createConnectionProvider(workingDir: String?): StreamConnectionProvider {
+        return ProcessStreamConnectionProvider(buildCommandLine(), workingDir)
+    }
+
+    private fun getServerFilePath(): String {
+        val pluginId = PluginId.getId("de.ricardoboss.plugins.hilfe")
+
+        PluginManager.getInstance().findEnabledPlugin(pluginId)?.let {
+            val pluginRootPath = PathManager.getPluginsPath()
+            val pluginName = it.name
+
+            return Paths.get(pluginRootPath, pluginName, "HILFE.LSP.Server.dll").pathString
+        }
+
+        throw Exception("Could not find plugin")
+    }
+
+    private fun buildCommandLine(): List<String> {
+        return listOf(
+            "dotnet",
+            getServerFilePath(),
+        )
+    }
+}
--- src/main/kotlin/de/ricardoboss/plugins/hilfe/MyPreloadingActivity.kt
+++ src/main/kotlin/de/ricardoboss/plugins/hilfe/MyPreloadingActivity.kt
@@ -3,10 +3,9 @@ package de.ricardoboss.plugins.hilfe
 import com.intellij.openapi.application.PreloadingActivity
 import com.intellij.openapi.progress.ProgressIndicator
 import org.wso2.lsp4intellij.IntellijLanguageClient
-import org.wso2.lsp4intellij.client.languageserver.serverdefinition.RawCommandServerDefinition
 
 class MyPreloadingActivity : PreloadingActivity() {
     override fun preload(indicator: ProgressIndicator) {
-        IntellijLanguageClient.addServerDefinition(RawCommandServerDefinition("hil", arrayOf<String>("/Users/ricardo/repos/ricardoboss/HILFE/HILFE.LSP.Server/bin/Debug/net7.0/HILFE.LSP.Server")))
+        IntellijLanguageClient.addServerDefinition(HilfeServerDefinition())
     }
 }
